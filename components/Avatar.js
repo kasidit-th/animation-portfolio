af23daea0cffb33d8049d8565bbd64ca673a1f15
Kasidit Thong-on <141108371+kasidit-th@users.noreply.github.com>
2024-09-23 20:01:21 +0700

    .

---
+++ components/Avatar.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+
+const Avatar = () => {
+  return <div className='hidden xl:flex xl:max-w-none'>
+<img src={'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/da908870-662b-4ce0-bcfd-6f4d5a6f7eca/dcx7amj-abc92daf-6ae5-4dea-9af9-90ecd29baf40.png/v1/fit/w_375,h_869/oregairu_oregairuzoku_07_render_by_aena34_dcx7amj-375w.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTAyMCIsInBhdGgiOiJcL2ZcL2RhOTA4ODcwLTY2MmItNGNlMC1iY2ZkLTZmNGQ1YTZmN2VjYVwvZGN4N2Ftai1hYmM5MmRhZi02YWU1LTRkZWEtOWFmOS05MGVjZDI5YmFmNDAucG5nIiwid2lkdGgiOiI8PTQ0MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.iF8gPbOOCwuqP_LEKXU143JIOgeF7HKqYECeEufrJ7E'} alt="" className='translate-z-0 w-full h-full' style={{ width: 600, height: 1300 }} />
+  </div>;
+};
+
+export default Avatar;
